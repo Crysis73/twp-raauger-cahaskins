8cfc68933065b61da0a045c42acb050cde58e52c
Crysis73 <36515767+Crysis73@users.noreply.github.com>
2018-09-08 15:20:24 -0400

    pass test 'CountRevisions'

--- src/main/java/RevisionParser.java
+++ src/main/java/RevisionParser.java
@@ -1,3 +1,8 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
--- src/test/java/testTWP.java
+++ src/test/java/testTWP.java
@@ -1,9 +1,11 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class testTWP {
     @Test
     public void testRevisionParser(){
-
-
+        RevisionParser parser = new RevisionParser();
+        int count = parser.countRevisions();
+        Assert.assertEquals(4,count);
     }
 }
