87e1e3487a941d199897213d6979f21a28815c06
Crysis73 <36515767+Crysis73@users.noreply.github.com>
2018-09-11 17:34:46 -0400

    Test hello

--- src/test/java/testTWP.java
+++ src/test/java/testTWP.java
@@ -1,12 +1,39 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 public class testTWP {
+
     @Test
-    public void testRevisionParser(){
+    public void testgetRevisions(){
         RevisionParser parser = new RevisionParser();
-        int count = parser.countRevisions();
-        Assert.assertEquals(4,count);
-        //hello
+        JsonArray array = parser.getRevisions();
+
+    }
+    @Test
+    public void testgetUsername(){
+        RevisionParser parser = new RevisionParser();
+        JsonArray array = parser.getRevisions();
+        String result = parser.getUsername(array,0);
+        Assert.assertEquals("SemiHypercube",result);
+    }
+    @Test
+    public void testgetTimestamp(){
+        RevisionParser parser = new RevisionParser();
+        JsonArray array = parser.getRevisions();
+        String result = parser.getTimestamp(array,0);
+        Assert.assertEquals("2018-08-14T20:34:45Z",result);
+    }
+    @Test
+    public void testgetRevisionList(){
+        RevisionParser parser = new RevisionParser();
+        JsonArray array = parser.getRevisions();
+        List<Revision> revisionList = parser.getRevisionList(array);
+        for(int i =0;i<revisionList.size();i++) {
+            System.out.println(revisionList.get(i).toString());
+        }
     }
 }
